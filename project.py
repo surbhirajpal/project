fe7f1c16145eef0691fd8931d6a732ec34b12f02
surbhirajpal <39900565+surbhirajpal@users.noreply.github.com>
2018-07-07 16:54:13 +0530

    Update project.py

--- project.py
+++ project.py
@@ -8,7 +8,9 @@ from PIL import Image,ImageTk
 
 root=Tk()
 root.geometry("1500x500")
-#table=sqlite3.connect("restaurant1.db")
+root.title("Restaurant Management System")
+
+#table=sqlite3.connect("restaurant8.db")
 #table.execute('''CREATE TABLE ORDERS
 #(ORDER_NUMBER INT PRIMARY KEY NOT NULL,FRIES_MEAL INT,LUNCH_MEAL INT,BURGER_MEAL INT,PIZZA_MEAL INT,CHEESE_BURGER INT,DRINKS INT,COST INT,SERVICE_CHARGE FLOAT,TAX FLOAT,SUBTOTAL FLOAT,TOTAL FLOAT);''')
 #table.commit()
@@ -16,21 +18,22 @@ root.geometry("1500x500")
 def price():
     master = Tk()
     master.geometry("550x650")
+    master.title("Price List")
     f1 = Frame(master)
     f1.pack(side=TOP)
-    x = Label(f1, text="PRICE LIST\n",font=("comic sans ms","20","underline","bold"),fg="steel blue")
+    x = Label(f1, text="PRICE LIST\n",font=("comic sans ms","15","underline","bold"),fg="steel blue")
     x.pack()
 
     f2 = Frame(master)
     f2.pack(side=LEFT)
-    a1 = Label(f2, text="ITEMS\n",font=("comic sans ms","15","underline"),fg="steel blue")
+    a1 = Label(f2, text="ITEMS\n",font=("comic sans ms","10","underline"),fg="steel blue")
     a1.pack()
     a = Label(f2, text="FRIES MEAL\n\nLUNCH MEAL\n\nBURGER MEAL\n\nPIZZA MEAL\n\nCHESSE BURGER\n\nDRINKS\n\nMEAL 1-\nFRIES MEAL+BURGER MEAL+DRINKS\n\nMEAL 2-\nPIZZA MEAL+CHESSE BURGER+DRINKS",font=("comic sans ms","15"),fg="steel blue")
     a.pack()
 
     f3 = Frame(master)
     f3.pack(side=RIGHT)
-    b1 = Label(f3, text="PRICE\n",font=("comic sans ms","15","underline"),fg="steel blue")
+    b1 = Label(f3, text="PRICE\n",font=("comic sans ms","10","underline"),fg="steel blue")
     b1.pack()
     b = Label(f3, text="Rs 100\n\nRs 230\n\nRs 155\n\nRs 440\n\nRs 150\n\nRs 50\n\n\nRs 250\n\n\nRs 500",font=("comic sans ms","15"),fg="steel blue")
     b.pack()
@@ -62,6 +65,14 @@ def amount():
 
 
 ran=StringVar()
+fries=StringVar()
+lunch=StringVar()
+burger=StringVar()
+pizza=StringVar()
+cheese_burger=StringVar()
+drinks=StringVar()
+meal_1=StringVar()
+meal_2=StringVar()
 cost=StringVar()
 service=StringVar()
 tax=StringVar()
@@ -73,20 +84,29 @@ order=str(x)
 ran.set(order)
 
 
+fries.set(0)
+lunch.set(0)
+burger.set(0)
+pizza.set(0)
+cheese_burger.set(0)
+drinks.set(0)
+meal_1.set(0)
+meal_2.set(0)
 
 def reset():
-    e2.delete(0,END)
-    e3.delete(0,END)
-    e4.delete(0,END)
-    e5.delete(0,END)
-    e6.delete(0,END)
-    e7.delete(0,END)
-    m1.delete(0, END)
-    m2.delete(0, END)
-    cost.set("")
-    service.set("")
-    tax.set("")
-    final.set("")
+    ran.set(int(order)+1)
+    fries.set(0)
+    lunch.set(0)
+    burger.set(0)
+    pizza.set(0)
+    cheese_burger.set(0)
+    drinks.set(0)
+    meal_1.set(0)
+    meal_2.set(0)
+    cost.set(0)
+    service.set(0)
+    tax.set(0)
+    final.set(0)
 
 #****************************************************************
 
@@ -106,37 +126,37 @@ frame2.pack(side=LEFT)
 
 lbl1=Label(frame2,text="Order No.",font=("comic sans ms","15"),fg="steel blue")
 lbl1.grid(row=0)
-e1=Entry(frame2,text="Order No.",textvariable=ran,font=("comic sans ms","15"),fg="steel blue")
+e1=Entry(frame2,textvariable=ran,font=("comic sans ms","15"),fg="steel blue")
 e1.grid(row=0,column=1)
 
 lbl2=Label(frame2,text="Fries Meal",font=("comic sans ms","15"),fg="steel blue")
 lbl2.grid(row=1)
-e2=Entry(frame2,font=("comic sans ms","15"),fg="steel blue")
+e2=Entry(frame2,textvariable=fries,font=("comic sans ms","15"),fg="steel blue")
 e2.grid(row=1,column=1)
 
 lbl3=Label(frame2,text="Lunch Meal",font=("comic sans ms","15"),fg="steel blue")
 lbl3.grid(row=2)
-e3=Entry(frame2,font=("comic sans ms","15"),fg="steel blue")
+e3=Entry(frame2,textvariable=lunch,font=("comic sans ms","15"),fg="steel blue")
 e3.grid(row=2,column=1)
 
 lbl4=Label(frame2,text="Burger Meal",font=("comic sans ms","15"),fg="steel blue")
 lbl4.grid(row=3)
-e4=Entry(frame2,font=("comic sans ms","15"),fg="steel blue")
+e4=Entry(frame2,textvariable=burger,font=("comic sans ms","15"),fg="steel blue")
 e4.grid(row=3,column=1)
 
 lbl5=Label(frame2,text="Pizza Meal",font=("comic sans ms","15"),fg="steel blue")
 lbl5.grid(row=4)
-e5=Entry(frame2,font=("comic sans ms","15"),fg="steel blue")
+e5=Entry(frame2,textvariable=pizza,font=("comic sans ms","15"),fg="steel blue")
 e5.grid(row=4,column=1)
 
 lbl6=Label(frame2,text="Cheese Burger",font=("comic sans ms","15"),fg="steel blue")
 lbl6.grid(row=5)
-e6=Entry(frame2,font=("comic sans ms","15"),fg="steel blue")
+e6=Entry(frame2,textvariable=cheese_burger,font=("comic sans ms","15"),fg="steel blue")
 e6.grid(row=5,column=1)
 
 meal1=Label(frame2,text="Meal 1",font=("comic sans ms","15"),fg="steel blue")
 meal1.grid(row=6)
-m1=Entry(frame2,font=("comic sans ms","15"),fg="steel blue")
+m1=Entry(frame2,textvariable=meal_1,font=("comic sans ms","15"),fg="steel blue")
 m1.grid(row=6,column=1)
 #------------------------------------
 
@@ -145,12 +165,12 @@ frame3.pack(side=RIGHT)
 
 meal2=Label(frame3,text="Meal 2",font=("comic sans ms","15"),fg="steel blue")
 meal2.grid(row=0)
-m2=Entry(frame3,font=("comic sans ms","15"),fg="steel blue")
+m2=Entry(frame3,textvariable=meal_2,font=("comic sans ms","15"),fg="steel blue")
 m2.grid(row=0,column=1)
 
 lbl7=Label(frame3,text="Drinks",font=("comic sans ms","15"),fg="steel blue")
 lbl7.grid(row=1)
-e7=Entry(frame3,font=("comic sans ms","15"),fg="steel blue")
+e7=Entry(frame3,textvariable=drinks,font=("comic sans ms","15"),fg="steel blue")
 e7.grid(row=1,column=1)
 
 lbl8=Label(frame3,text="Cost",font=("comic sans ms","15"),fg="steel blue")
@@ -179,9 +199,9 @@ e12=Entry(frame3,textvariable=final,font=("comic sans ms","15"),fg="steel blue")
 e12.grid(row=6,column=1)
 
 
-#sheet.write()
-# table.execute("INSERT INTO ORDERS VALUES({},{},{},{},{},{},{},{},{},{},{},{})".format(e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12))
-# table.commit()
+
+#table.execute("INSERT INTO ORDERS VALUES({},{},{},{},{},{},{},{},{},{},{},{})VALUES(e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12);")
+#table.commit()
 
 #---------------------------------
 
@@ -193,7 +213,7 @@ total=Button(frame4,text="total",height=3,width=10,font=("comic sans ms","11"),f
 total.grid(row=0,column=5,padx=10,pady=10)
 reset=Button(frame4,text="reset",height=3,width=10,font=("comic sans ms","11"),fg="steel blue",command=reset)
 reset.grid(row=0,column=10,padx=10,pady=10)
-exit=Button(frame4,text="exit",height=3,width=10,font=("comic sans ms","11"),fg="steel blue",command=root.quit)
+exit=Button(frame4,text="exit",height=3,width=10,font=("comic sans ms","11"),fg="steel blue",command=quit)
 exit.grid(row=0,column=15,padx=10,pady=10)
 
 #--------------------------------------------------------
